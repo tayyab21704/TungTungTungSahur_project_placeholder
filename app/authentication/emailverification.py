365bcd50a598e26afd890992576e80df193bcb84
Netraj Patil <netrajpatil12mati@gmail.com>
2025-08-24 16:46:48 +0530

    authentication

---
+++ app/authentication/emailverification.py
@@ -0,0 +1,8 @@
+import re
+
+def validate_email(email: str) -> bool:
+    """
+    Comprehensive email validation
+    """
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(email_regex, email))
